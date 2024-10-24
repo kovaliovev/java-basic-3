3a6ea62ff8c1d0c14e906871d45c1e67b468325f
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-24 09:44:41 +0300

    Override EducationalInstitution comparison methods

--- src/main/java/org/example/EducationalInstitution.java
+++ src/main/java/org/example/EducationalInstitution.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class EducationalInstitution {
 
     private final String name;
@@ -34,5 +36,33 @@ public class EducationalInstitution {
 
     public String getType() {
         return type;
+    }
+
+    @Override
+    public String toString() {
+        return "EducationalInstitution { " +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", establishedYear=" + establishedYear +
+                ", numberOfStudents=" + numberOfStudents +
+                ", type='" + type + '\'' +
+                " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EducationalInstitution that = (EducationalInstitution) o;
+        return establishedYear == that.establishedYear &&
+                numberOfStudents == that.numberOfStudents &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(address, that.address) &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address, establishedYear, numberOfStudents, type);
     }
 }
