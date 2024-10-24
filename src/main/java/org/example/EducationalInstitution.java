35adb2f64469b33af15276e0cee3d95f713fe3b6
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-24 09:48:36 +0300

    Add JavaDoc documentation

--- src/main/java/org/example/EducationalInstitution.java
+++ src/main/java/org/example/EducationalInstitution.java
@@ -2,6 +2,14 @@ package org.example;
 
 import java.util.Objects;
 
+/**
+ * The {@code EducationalInstitution} class represents an educational institution with basic attributes such as
+ * name, address, established year, number of students, and type.
+ * <p>
+ * This class provides methods to retrieve the details of the institution and overrides {@code toString},
+ * {@code equals}, and {@code hashCode} methods.
+ * </p>
+ */
 public class EducationalInstitution {
 
     private final String name;
@@ -10,6 +18,15 @@ public class EducationalInstitution {
     private final int numberOfStudents;
     private final String type;
 
+    /**
+     * Constructs an {@code EducationalInstitution} instance with the specified attributes.
+     *
+     * @param name             the name of the educational institution
+     * @param address          the address of the educational institution
+     * @param establishedYear  the year the institution was established
+     * @param numberOfStudents the number of students enrolled in the institution
+     * @param type             the type of educational institution (e.g., university, high school)
+     */
     public EducationalInstitution(String name, String address, int establishedYear, int numberOfStudents, String type) {
         this.name = name;
         this.address = address;
@@ -18,26 +35,57 @@ public class EducationalInstitution {
         this.type = type;
     }
 
+    /**
+     * Returns the name of the educational institution.
+     *
+     * @return the name of the institution
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the address of the educational institution.
+     *
+     * @return the address of the institution
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Returns the year the educational institution was established.
+     *
+     * @return the year of establishment
+     */
     public int getEstablishedYear() {
         return establishedYear;
     }
 
+    /**
+     * Returns the number of students enrolled in the educational institution.
+     *
+     * @return the number of students
+     */
     public int getNumberOfStudents() {
         return numberOfStudents;
     }
 
+    /**
+     * Returns the type of the educational institution (e.g., university, high school).
+     *
+     * @return the type of the institution
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Returns a string representation of the educational institution, including its name, address,
+     * established year, number of students, and type.
+     *
+     * @return a string representation of the institution
+     */
     @Override
     public String toString() {
         return "EducationalInstitution { " +
@@ -49,6 +97,14 @@ public class EducationalInstitution {
                 " }";
     }
 
+    /**
+     * Compares this educational institution to the specified object. The result is {@code true} if and only if the
+     * argument is not {@code null} and is an {@code EducationalInstitution} object that represents the same
+     * institution with the same attributes.
+     *
+     * @param o the object to compare this institution against
+     * @return {@code true} if the given object represents an equivalent institution; {@code false} otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -61,6 +117,11 @@ public class EducationalInstitution {
                 Objects.equals(type, that.type);
     }
 
+    /**
+     * Returns a hash code for this educational institution based on its attributes.
+     *
+     * @return a hash code value for the institution
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, address, establishedYear, numberOfStudents, type);
