1b846654830dc41819ee5afbc6ea0f16e3afe20c
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-24 09:43:22 +0300

    Implement Educational Institution class

--- src/main/java/org/example/EducationalInstitution.java
+++ src/main/java/org/example/EducationalInstitution.java
@@ -1,1 +1,38 @@
 package org.example;
+
+public class EducationalInstitution {
+
+    private final String name;
+    private final String address;
+    private final int establishedYear;
+    private final int numberOfStudents;
+    private final String type;
+
+    public EducationalInstitution(String name, String address, int establishedYear, int numberOfStudents, String type) {
+        this.name = name;
+        this.address = address;
+        this.establishedYear = establishedYear;
+        this.numberOfStudents = numberOfStudents;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getEstablishedYear() {
+        return establishedYear;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
